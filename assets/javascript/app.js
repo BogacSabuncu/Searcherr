cbc3f2cbabb148767b89108d82f38b494c425c48
Uncle Sam <djiasara@gmail.com>
2019-05-02 12:57:45 -0400

    normalizing api call result

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,58 @@
 $(document).ready(function(){
+    
+
+    // let jobObj = {
+    //     jobTitle: " ",
+    //     jobCompany: " ",
+    //     joblocation = {
+    //         country = "",
+    //         city = "",
+    //         lat = 0,
+    //         long = 0
+    //     },
+    //     jobUrl = "",
+    //     jobSalaryMin = "",
+    //     jobSalaryMax = "",
+    //     jobDescription = " "
+    //  }
+     function jobDisplay(job){
+        return (`
+        // <div>
+        //     <h2>${job.title}</h2>
+        //     <h2>${job.company}</h2>
+        //     <h4>${job.location.countr} ${job.location.city}</h4>
+        //     <h4>${job.company}</h4>
+        //     <h4>${job.salary}</h4>
+        //     <p>${job.description}</p>
+        // </div>
+        // <button >Yes</button>
+        // <button>No</button>
+        `);
+    }
+    function  normalizeJob(usajob){
+        let jobNorm = {};
+            jobNorm.title = usajob.PositionTitle;
+            jobNorm.company = usajob.OrganizationName;
+            jobNorm.location = {
+                city: usajob.PositionLocation[0].LocationName,
+                country: usajob.PositionLocation[0].CountryCode,
+                lat: usajob.PositionLocation[0].Longitude,
+                long: usajob.PositionLocation[0].Latitude,
+
+            }
+            
+            jobNorm.url = usajob.PositionURI; 
+            jobNorm.salaryMin = usajob.PositionRemuneration[0].MinimumRange;
+            jobNorm.salaryMax  = usajob.PositionRemuneration[0].MaximumRange;
+            jobNorm.description = usajob.UserArea.Details.JobSummary;
+            jobNorm.qualification = usajob.QualificationSummary;
+            jobNorm.startDate = usajob.PublicationStartDate;
+            jobNorm.closeDate = usajob.ApplicationCloseDate;
+
+        return jobNorm;
+        
+    }
+    const USAJobs = [];
     const BASEURL_USAJOBS= 'https://data.usajobs.gov/api/Search?';
     $.ajax({
         headers:{
@@ -8,12 +62,11 @@ $(document).ready(function(){
         data:{PositionTitle:"full stack developer", Page:0,ResultsPerPage:50, location:"Atlanta", Radius:75},
         method: "GET"
     }).then(function(jobs){
+        console.log(jobs);
         const resultList = jobs.SearchResult.SearchResultItems.map(function(result){
-            console.log("--------------------------------");
-            console.log(JSON.stringify(result));
-            console.log("--------------------------------");
-        })
-        console.log(resultList);
+            return normalizeJob(result.MatchedObjectDescriptor);
+        });
+        console.log(JSON.stringify(resultList));
     }).catch(function(err){
         console.log(error);
     })
